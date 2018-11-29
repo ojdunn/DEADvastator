aeba5b345ca34ae943543dc7b7d35400bb388c12
dunnow16 <dunnow@mail.gvsu.edu>
2018-11-29 06:27:08 -0500

    title screen

---
+++ app/src/main/java/edu/gvsu/cis/deadvastation/TitleScreenActivity.java
@@ -0,0 +1,13 @@
+package edu.gvsu.cis.deadvastation;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TitleScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_title_screen);
+    }
+}
